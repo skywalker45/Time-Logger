ee22e131880398ff9bc0ccda8d436c9b1582fdae
Daniel Salami <daniel.salami@fotlinc.com>
2019-12-28 16:27:14 -0600

    commiting changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { PtoComponent } from './pto/pto.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MenuComponent },
+  { path: 'addPto', component: PtoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { PtoComponent } from './pto/pto.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,10 +13,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-      { path: 'addPto', component: PtoComponent}
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
